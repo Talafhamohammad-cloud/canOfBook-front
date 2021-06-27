90066187caad1441ca3f6cc2d40112268db0fab7
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-06-27 18:55:40 +0300

    finish front

---
+++ canof-book/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Header from './Header';
+import { withAuth0 } from '@auth0/auth0-react';
+import IsLoadingAndError from './IsLoadingAndError';
+import Login from './Login';
+import Footer from './Footer';
+import Profile from './Profile';
+import MyFavoriteBooks from './MyFavoriteBooks';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+
+  render() {
+    console.log('app', this.props);
+    const { isAuthenticated } = this.props.auth0;
+    return (
+      <div>
+        <Router>
+          <IsLoadingAndError>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+               
+                {
+                  isAuthenticated ? < MyFavoriteBooks / > : < Login / >
+                }
+
+              </Route>
+              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              {isAuthenticated && <Profile />}
+            </Switch>
+            <Footer />
+          </IsLoadingAndError>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default withAuth0(App);
---
+++ canof-book/src/BestBook.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+//import axios from 'axios';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button';
+
+export class BestBook extends Component {
+// eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Card style={{ width: '18rem' }}>
+
+                    <Card.Title>My Books</Card.Title>
+                    {this.props.userData.length > 0 && this.props.userData.map((book, idx) => (
+                        <Card.Body>
+                            <Card.Text>
+                                {book.name}
+
+                            </Card.Text>
+                            <Button onClick={(e) => this.props.deleteBook(idx)}
+                                variant="primary" type="submit">
+                                Delete Book
+                            </Button>
+                            <Button onClick={(e) => this.props.showUpdateForm(book,idx)}
+                                variant="primary" type="submit">
+                                Edit
+                            </Button>
+                        </Card.Body>
+                    ))}
+
+                </Card>
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default BestBook
